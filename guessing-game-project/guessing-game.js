00e41fe41dcc991189f9bcc795e8c9165c1bb81b
“Matt <matt.s.demartino@gmail.com>
2022-08-03 20:13:38 -0400

    Intitialized game

---
+++ guessing-game-project/guessing-game.js
@@ -0,0 +1,49 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const randomInRange = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const askRange = () => {
+  rl.question("Enter a min number: ", (minNumber) => {
+    let minRange = Number(minNumber);
+    rl.question("Enter a max number: ", (maxNumber) => {
+      let maxRange = Number(maxNumber);
+      console.log(
+        `I'm thinking of a number between ${minNumber} and ${maxNumber}`
+      );
+      secretNumber = randomInRange(maxRange, minRange);
+      return askGuess();
+    });
+  });
+};
+
+const checkGuess = (number) => {
+  if (number > secretNumber) {
+    console.log("Too high");
+    return false;
+  } else if (number < secretNumber) {
+    console.log("Too low");
+    return false;
+  } else if (number === secretNumber) {
+    console.log("Correct!");
+    return true;
+  }
+};
+
+const askGuess = () => {
+  rl.question("Enter a guess ", (answer) => {
+    if (checkGuess(Number(answer)) === true) {
+      console.log("You win!");
+      rl.close();
+    } else {
+      askGuess();
+    }
+  });
+};
+
+return askRange();
