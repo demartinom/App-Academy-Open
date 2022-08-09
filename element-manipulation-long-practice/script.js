34addda61f932ee9b4150629a8f8d4f639b27314
“Matt <matt.s.demartino@gmail.com>
2022-08-09 12:01:39 -0400

    created overall function

--- element-manipulation-long-practice/script.js
+++ element-manipulation-long-practice/script.js
@@ -1,24 +1,25 @@
-const addName = () => {
-  const newElement = document.createElement("h1");
-  newElement.innerText = "Matt DeMartino";
-  document.body.appendChild(newElement);
-  let name = document.querySelector("h1");
-  name.setAttribute("id", "name");
-};
+const newFunctions = () => {
+  const addName = () => {
+    const newElement = document.createElement("h1");
+    newElement.innerText = "Matt DeMartino";
+    document.body.appendChild(newElement);
+    let name = document.querySelector("h1");
+    name.setAttribute("id", "name");
+  };
 
-const addUL = () => {
-  const newElement = document.createElement("ul");
-  document.body.appendChild(newElement);
-  let name = document.querySelector("ul");
-  name.setAttribute("id", "list");
-};
+  const addUL = () => {
+    const newElement = document.createElement("ul");
+    document.body.appendChild(newElement);
+    let name = document.querySelector("ul");
+    name.setAttribute("id", "list");
+  };
 
-const addLI = (text) => {
-  const list = document.querySelector("ul");
-  let newElement = list.createElement("li");
-  newElement.innerText = text;
-  list.appendChild(newElement);
+  const addLI = () => {
+    const list = document.querySelector("ul");
+    
+  };
+  addName()
+  addUL()
+  addLI()
 };
-window.onload = addName;
-addUL();
-addLI('I live in America')
+window.onload = newFunctions;
